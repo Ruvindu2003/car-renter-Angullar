66f88d48fc4afe8f6cfa71c0d7e4a564f9860b43
Ruvindu <ruvindusharada22@gmail.com>
2025-03-13 01:01:47 +0530

    conneect the api and creat the Loging form

--- src/app/auth/components/signup/signup.component.ts
+++ src/app/auth/components/signup/signup.component.ts
@@ -1,51 +1,62 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { id_ID } from 'ng-zorro-antd/i18n';
+import { AuthService } from '../../services/auth/auth.service'; 
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink,ReactiveFormsModule],
+  standalone: true, 
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css'] 
 })
 export class SignupComponent {
+  isSpinning: boolean = false; 
+  signupForm!: FormGroup; 
 
-  isSpinnnging: boolean=false;
+  constructor(private fb: FormBuilder, private authservice: AuthService) {}
 
-  SiginForme!:FormGroup
-  constructor(private fb:FormBuilder){
-
-  }
-  ngOnInit(){
-    this.SiginForme = this.fb.group({
+  ngOnInit() {
+    this.signupForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      name: ['',[Validators.required]],
-      password:['',[Validators.required]],
-      confirmpassword:['',[Validators.required,this.confirmationValidate]],
-       })
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmpassword: ['', [Validators.required, this.confirmationValidate.bind(this)]] 
+    });
   }
-
 
   confirmationValidate = (control: FormControl): { [s: string]: boolean } | null => {
-   
     if (!control.value) {
       return { required: true };
     }
 
-    if (control.value !== this.SiginForme.controls["password"].value) {
+    if (control.value !== this.signupForm?.controls['password'].value) {
       return { confirm: true, error: true };
     }
-  
- 
+
     return null;
   };
 
-
-  Register(){
-    console.log(this.SiginForme.value);
-    
-
+  register() {
+    if (this.signupForm.invalid) {
+      console.error('Form is invalid');
+      return;
     }
 
+    this.isSpinning = true; 
+    console.log(this.signupForm.value);
+
+    this.authservice.rejister(this.signupForm.value).subscribe({
+      next: (res) => {
+        this.isSpinning = false; 
+        console.log('Registration successful', res);
+
+      },
+      error: (err) => {
+        this.isSpinning = false; 
+        console.error('Registration failed', err);
+        
+      }
+    });
+  }
 }
--- src/app/auth/services/auth/auth.service.ts
+++ src/app/auth/services/auth/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+const BASICUrl=["http://localhost:8080"]
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
+  rejister(signupRequest:any):Observable<any>{
+    return this.http.post(BASICUrl+"/api/auth/signup",signupRequest)
+  }
 }
